74badcb25f6ff7d50e90b0cea5199aba8f14c322
syseoz <syseoz@naver.com>
2023-03-28 12:07:22 +0900

    DispatcherServlet

--- src/main/java/com/example/helloboot/HelloController.java
+++ src/main/java/com/example/helloboot/HelloController.java
@@ -1,7 +1,12 @@
 package com.example.helloboot;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 import java.util.Objects;
 
+@RequestMapping("/hello")
 public class HelloController {
 
     private final HelloService helloService;
@@ -13,6 +18,8 @@ public class HelloController {
     }
 
 
+    @GetMapping
+    @ResponseBody
     public String hello(String name){
         return helloService.sayHello(Objects.requireNonNull(name));
     }
--- src/main/java/com/example/helloboot/HellobootApplication.java
+++ src/main/java/com/example/helloboot/HellobootApplication.java
@@ -11,6 +11,8 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -23,8 +25,7 @@ public class HellobootApplication {
 
 	public static void main(String[] args) {
 		//스프링컨테이너 사용
-		//애플리케이션 컨텍스중 코드에 의해 쉽게 만들수 있는 객체
-		GenericApplicationContext applicationContext = new GenericApplicationContext();
+		GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
 		applicationContext.registerBean(HelloController.class);//빈등록,어떤 객체보단 클래스 메타정보를 넣어준다, getBean으로 반환
 		applicationContext.registerBean(SimpleHelloService.class);//빈 등록 순서는 컨테이너가 자동으로 처리한다
 		applicationContext.refresh();//컨테이너 초기화
@@ -34,32 +35,11 @@ public class HellobootApplication {
 		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
 		WebServer webServer = serverFactory.getWebServer(servletContext -> {
 			//서블릿등록
-			servletContext.addServlet("frontController", new HttpServlet() {
-				@Override
-				protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-					//인증, 보안, 다국어, 공동 기능...
-
-					//매핑
-					if(req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())){
-						String name = req.getParameter("name");
-
-						HelloController helloController = applicationContext.getBean(HelloController.class);
-						String result = helloController.hello(name);//바인딩
-//
-//						resp.setStatus(HttpStatus.OK.value());
-//						resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
-						resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
-						resp.getWriter().println(result);
-					} else if (req.getRequestURI().equals("/user")) {
-
-					}else {
-						resp.setStatus(HttpStatus.NOT_FOUND.value());
-					}
-
-				}
-			}).addMapping("/*");//front-controller 역활
+			servletContext.addServlet("dispatcherServlet",
+					new DispatcherServlet(applicationContext)
+			).addMapping("/*");
 		});
-		
+
 		webServer.start();//톰캣 서브릿 컨테이너 실행
 	}
 
